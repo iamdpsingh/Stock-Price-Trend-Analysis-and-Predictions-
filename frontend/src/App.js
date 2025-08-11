f238685dc90646f1c50fcc060e1812b97f8d11c2
dpsingh <dhruvpratapsingh30.official2.o@gmail.com>
2025-08-11 07:52:48 +0530

    Re-design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { AuthProvider } from "./contexts/AuthContext";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -7,6 +7,8 @@ import Dashboard from "./components/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 import AdminRoute from "./components/AdminRoute";
 import AdminDashboard from "./components/AdminDashboard";
+import StocksPage from './pages/StocksPage'; 
+
 
 import "./styles/darkTheme.css";
 
@@ -34,6 +36,12 @@ function App() {
               </AdminRoute>
             }
           />
+          <Route path="/stocks" element={
+            <ProtectedRoute>
+              <StocksPage />
+            </ProtectedRoute>
+          } />
+
         </Routes>
       </Router>
     </AuthProvider>
